cc2591587277e6837b9a3f402e97f9eec37df1bb
slayonce <katharinemg@gmail.com>
2023-04-09 19:36:00 -0400

    Added html, js, css files and outlined sections

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+"use strict";
+// // SECTION: QUESTIONS:
+// 1) Boolean is:
+// a. a function
+// b. a logical data type that is true or false
+// c. a symbol
+// d. a data type that is a sequence of characters
+// 2) Which of the following is accurate?
+// a. 3 === three
+// b. 3 !== 3
+// c. true && true is false
+// d. true || false is true
+// 3) x++ is the same as saying
+// a. x + 1
+// b. x + 2
+// c. x^2
+// c. x * 2
+// 4) What do you enclose a string in?
+// a. Curly brackets {}
+// b. Parentheses ()
+// c. Brackets []
+// d. Quotations ""
+// 5) In the array: var numbers = [1, 2, 3, 4]; what is the index of 3?
+// a. 1
+// b. 2
+// c. 3
+// d. 4
+
+// // SECTION: SELECTING TODO:
+
+// // SECTION: START
+// TODO: 1) click ||
+
+// // SECTION: QUESTIONS
+
+// // SECTION: CLICK ANSWER 1) Correct 2) Incorrect
+
+// // SECTION: TIMER
+
+// // SECTION: SAVE HIGH SCORE
+
+// // SECTION: NEW GAME
